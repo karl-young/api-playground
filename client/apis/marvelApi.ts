97ab2cd3062a862f2f5d525bc24fadf2fcae089f
Karl Young <Karlyoung112@gmail.com>
2024-01-24 13:15:00 +1300

    Fixed models

--- client/apis/marvelApi.ts
+++ client/apis/marvelApi.ts
@@ -1,9 +1,9 @@
 import request from 'superagent'
-import { Comic } from '../../models/Comic.ts'
+import { Result } from '../../models/Comic.ts'
 
 const serverURL = '/api/v1'
 
-export async function getComics(): Promise<Comic[]> {
+export async function getComics(): Promise<Result[]> {
   const response = await request.get(`${serverURL}/comics`)
   const jsonINfo = JSON.parse(response.body.text)
   return jsonINfo.data.results

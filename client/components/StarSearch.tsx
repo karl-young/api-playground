78a0f4bbe58a6776ff35659c4c1bdf8925da9dce
Karl Young <Karlyoung112@gmail.com>
2024-02-11 16:08:52 +1300

    Feat: Added Starwars api

---
+++ client/components/StarSearch.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { searchStarWars } from '../apis/star.ts'
+import {
+  Person,
+  Film,
+  Starship,
+  Vehicle,
+  Species,
+  Planet,
+} from '../../models/StarWars.ts'
+
+const StarSearch = () => {
+  const [query, setQuery] = useState('')
+  const [selection, setSelection] = useState('people')
+
+  const {
+    data: searchResults,
+    error,
+    isLoading,
+  } = useQuery(['searchStarWars', query, selection], async () => {
+    if (!query.trim()) {
+      return []
+    }
+    try {
+      return await searchStarWars(query, selection)
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message)
+      } else {
+        throw new Error('Something went wrong Api call')
+      }
+    }
+  })
+
+  if (isLoading) {
+    return (
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    )
+  }
+
+  if (error instanceof Error) {
+    return <p>Error: {error.message}</p>
+  }
+
+  const getResourceInfo = (
+    result: Person | Film | Starship | Vehicle | Species | Planet
+  ) => {
+    let info = ''
+
+    for (const [key, value] of Object.entries(result)) {
+      info += `${key}: ${value}\n`
+    }
+
+    return info
+  }
+
+  return (
+    <div>
+      <h1>Star Wars Search</h1>
+      <form id="search-form">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter search query..."
+        />
+        <select
+          value={selection}
+          onChange={(e) => setSelection(e.target.value)}
+        >
+          <option value="people">People</option>
+          <option value="films">Films</option>
+          <option value="starships">Starships</option>
+          <option value="vehicles">Vehicles</option>
+          <option value="species">Species</option>
+          <option value="planets">Planets</option>
+        </select>
+        <input type="submit" value="Search" />
+      </form>
+      <ul>
+        {searchResults &&
+          searchResults.map(
+            (result: Person | Film | Starship | Vehicle | Species | Planet) => (
+              <li className="star-result" key={result.url}>
+                <pre>{getResourceInfo(result)}</pre>
+              </li>
+            )
+          )}
+      </ul>
+    </div>
+  )
+}
+
+export default StarSearch

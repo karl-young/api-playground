29cf453954eea17eaeaff369da6873f3fb2bdf66
Karl Young <Karlyoung112@gmail.com>
2024-07-11 16:10:01 +1200

    exteded server

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
+  },
+  build: {
+    minify: 'terser', // Minify using terser
+    sourcemap: false, // Disable source maps in production
+    base: '/Api-Playground/', // Base URL for the application
+    assetsDir: 'assets', // Directory for static assets
   },
 })
